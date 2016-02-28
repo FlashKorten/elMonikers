bad71fd588a27bfc94ffd6362a05139ec08cce95
Sebastian Korten <Sebastian.Korten@neuland-bfi.de>
2016-02-28 11:26:43 +0100

    update delegates to specific update functions

--- ElMonikers.elm
+++ ElMonikers.elm
@@ -8,43 +8,76 @@ import Random exposing (initialSeed)
 import ElMonikers.Model exposing (..)
 import ElMonikers.View exposing (..)
 
+updateInit : Model -> Model
+updateInit m = let model = initialModel m.nextSeed
+               in { model | teams = resetTeamScores m.teams
+                  , maxCount = m.maxCount
+                  , count = m.count}
+
+updateSolved : Model -> Model
+updateSolved m = case m.cards of
+        ( 1, _, _, _ ) -> { m | teams = incScore m.teams
+                              , cards = removeElem m.cards
+                              , round = m.round + 1
+                              , state = End}
+        _              -> { m | teams = incScore m.teams
+                              , cards = removeElem m.cards }
+
+updateNotSolved : Model -> Model
+updateNotSolved m = { m | cards = nextElem m.cards }
+
+updateNextTeam : Model -> Model
+updateNextTeam m = { m | state = Play }
+
+updateNextRound : Model -> Model
+updateNextRound m = let (shuffledCards, seed) = reshuffleDiscards m
+                     in { m | state = Play
+                            , cards = shuffledCards
+                            , count = if m.round == 1 then m.maxCount else m.count
+                            , nextSeed = seed}
+
+updateTick : Model -> Model
+updateTick m = case m.state of
+                 Play -> case m.count of
+                           0 -> { m | teams = cycle m.teams
+                                    , cards = nextElem m.cards
+                                    , state = Switch
+                                    , count = m.maxCount }
+                           n -> { m | count = n - 1 }
+                 _    -> m
+
+updateIncTimer : Model -> Model
+updateIncTimer m = { m| maxCount = m.maxCount + 5}
+
+updateDecTimer : Model -> Model
+updateDecTimer m = { m | maxCount = if m.maxCount > 5 then m.maxCount - 5 else m.maxCount}
+
+updateAddTeam : Model -> Model
+updateAddTeam m = { m | teams = m.teams ++ [ newTeam "New Team" ]}
+
+updateRemoveTeam : Model -> Int -> Model
+updateRemoveTeam m n = { m | teams = (take n m.teams) ++ (drop (n + 1) m.teams)}
+
+updateUpdateName : Model -> Int -> String -> Model
+updateUpdateName m n name = { m | teams = (updateName n name m.teams)}
+
 update : Action -> Model -> Model
 update action model =
   case action of
-    NoOp    -> model
-    Init    -> let m = initialModel model.nextSeed
-                in { m | teams = resetTeamScores model.teams
-                       , maxCount = model.maxCount
-                       , count = model.count}
-    NotSolved   -> { model | cards = nextElem model.cards }
-    NextTeam -> { model | state = Play }
-    Solved  -> case model.cards of
-        ( 1, _, _, _ ) -> { model | teams = incScore model.teams
-                                  , cards = removeElem model.cards
-                                  , round = model.round + 1
-                                  , state = End}
-        _              -> { model | cards = removeElem model.cards
-                                  , teams = incScore model.teams }
-    NextRound -> let (shuffledCards, seed) = reshuffleDiscards model
-                  in {model | state = Play
-                            , cards = shuffledCards
-                            , count = if model.round == 1 then model.maxCount else model.count
-                            , nextSeed = seed}
+    NoOp              -> model
+    Init              -> updateInit model
+    Solved            -> updateSolved model
+    NotSolved         -> updateNotSolved model
+    NextTeam          -> updateNextTeam model
+    NextRound         -> updateNextRound model
     -- Configuration
-    IncTimer -> {model | maxCount = model.maxCount + 5}
-    DecTimer -> {model | maxCount = if model.maxCount > 5 then model.maxCount - 5 else model.maxCount}
-    AddTeam -> {model | teams = model.teams ++ [ newTeam "New Team" ]}
-    RemoveTeam n -> {model | teams = (take n model.teams) ++ (drop (n + 1) model.teams)}
-    UpdateName n name -> {model | teams = (updateName n name model.teams)}
+    IncTimer          -> updateIncTimer model
+    DecTimer          -> updateDecTimer model
+    AddTeam           -> updateAddTeam model
+    RemoveTeam n      -> updateRemoveTeam model n
+    UpdateName n name -> updateUpdateName model n name
     -- Timer Event
-    Tick    -> case model.state of
-                 Play -> case model.count of
-                           0 -> { model | teams = cycle model.teams
-                                        , cards = nextElem model.cards
-                                        , state = Switch
-                                        , count = model.maxCount }
-                           n -> { model | count = n - 1 }
-                 _    -> model
+    Tick              -> updateTick model
 
 -- PATCHING
 
