ae669c84837661efe193069ddabfec0deeaeb4bb
Sebastian Korten <Sebastian.Korten@neuland-bfi.de>
2016-02-29 21:20:44 +0100

    correctly threading the seed

--- ElMonikers/Shuffle.elm
+++ ElMonikers/Shuffle.elm
@@ -3,27 +3,27 @@ module ElMonikers.Shuffle (rShuffle) where
 import List exposing (foldl, repeat, length)
 import Random exposing (Seed, bool, generate)
 
-
 rShuffle : Int -> (List a, Seed) -> (List a, Seed)
 rShuffle n l = let fs = repeat n shuffle
                 in l |> foldl (<<) identity fs
 
 shuffle : (List a, Seed) -> (List a, Seed)
-shuffle (l, s) =
-  if length l < 2 then (l, s)
-                  else let ( l1, l2 ) = split l
-                        in merge ( shuffle (l1, s), shuffle (l2, s) )
+shuffle (l, seed) =
+  if length l < 2 then (l, seed)
+                  else let ( h1, h2 ) = split l
+                           ( shuffled1, tmpSeed) = shuffle (h1, seed)
+                           ( shuffled2, newSeed) = shuffle (h2, tmpSeed)
+                        in merge (shuffled1, shuffled2, newSeed)
 
-merge : ( (List a, Seed), (List a, Seed) ) -> (List a, Seed)
-merge input =
-  case input of
-    ( (xs,s) , ([],_)  ) -> (xs,s)
-    ( ([],_) , (ys,s)  ) -> (ys,s)
-    ( (x :: xs, s1), (y :: ys, s2) ) -> let (p, sn) = generate bool s1
-      in if p then let (m, sx) = (merge ( (xs, s2), ((y :: ys), sn) ))
-                    in (x :: m, sx)
-              else let (m, sx) = (merge ( ((x :: xs), s2), (ys, sn) ))
-                    in (y :: m, sx)
+merge : (List a, List a, Seed) -> (List a, Seed)
+merge t = case t of
+    ( xs,    [],    seed ) -> (xs, seed)
+    ( [],    ys,    seed ) -> (ys, seed)
+    ( x::xs, y::ys, seed ) -> let (p, tmpSeed) = generate bool seed
+      in if p then let (m, newSeed) = merge (xs, y :: ys, tmpSeed)
+                    in (x::m, newSeed)
+              else let (m, newSeed) = merge (x :: xs, ys, tmpSeed)
+                    in (y::m, newSeed)
 
 split : List a -> ( List a, List a )
 split xs = let go i = case i of
